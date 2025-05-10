d2d719040d8e7ba27de2d9a1b87b873d05ba41d4
ravmax-rdl <ravindudl.rc@gmail.com>
2025-05-10 18:28:57 +0530

    fix: Update Header component to improve logo styling and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,13 @@ const Header: React.FC = () => {
         <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/images/Flaire_bird.png" alt="Flaire Bird Logo" width={48} height={48} />
+            <Image
+              src="/images/Flaire_bird.png"
+              alt="Flaire Bird Logo"
+              width={48}
+              height={48}
+              className="min-w-fit w-10 h-10 object-contain"
+            />
             <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
               {siteDetails.siteName}
             </span>
