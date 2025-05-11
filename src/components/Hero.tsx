88f043a2713dc42e6c070866b925eb2f2527c753
ravmax-rdl <ravindudl.rc@gmail.com>
2025-05-11 12:30:31 +0530

    refactor: Update hero section text and adjust heading for campaign management branding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero: React.FC = () => {
       <div className="absolute left-0 right-0 bottom-0 backdrop-blur-[2px] h-40 bg-gradient-to-b from-transparent via-[rgba(233,238,255,0.5)] to-[rgba(202,208,230,0.5)]"></div>
 
       <div className="text-center">
-        <h1 className="font-funnel text-4xl md:text-6xl md:leading-tight font-bold text-foreground max-w-lg md:max-w-2xl mx-auto">
+        <h1 className="font-funnel text-4xl md:text-6xl md:leading-tight font-bold text-foreground max-w-lg md:max-w-3xl mx-auto">
           {heroDetails.heading}
         </h1>
         <p className="mt-4 text-foreground max-w-lg mx-auto">{heroDetails.subheading}</p>
