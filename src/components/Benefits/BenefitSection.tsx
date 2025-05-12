efb2209258ab41b987bfa00da2c654cffe8a3fe2
ravmax-rdl <ravindudl.rc@gmail.com>
2025-05-12 18:56:53 +0530

    feat: Replace Image component with TiltedCard in BenefitSection for enhanced visual presentation

--- src/components/Benefits/BenefitSection.tsx
+++ src/components/Benefits/BenefitSection.tsx
@@ -1,11 +1,11 @@
 "use client"
-import Image from "next/image";
 import clsx from "clsx";
 import { motion, Variants } from "framer-motion"
 
 import BenefitBullet from "./BenefitBullet";
 import SectionTitle from "../SectionTitle";
 import { IBenefit } from "@/types";
+import TiltedCard from "../TiltedCard";
 
 interface Props {
     benefit: IBenefit;
@@ -88,7 +88,7 @@ const BenefitSection: React.FC<Props> = ({ benefit, imageAtRight }: Props) => {
 
                 <div className={clsx("mt-5 lg:mt-0", { "lg:order-2": imageAtRight })}>
                     <div className={clsx("w-fit flex", { "justify-start": imageAtRight, "justify-end": !imageAtRight })}>
-                        <Image src={imageSrc} alt="title" width="384" height="762" quality={100} className="lg:ml-0" />
+                        <TiltedCard imageSrc={imageSrc} altText="title" width="384" height="762" quality={100} className="lg:ml-0" />
                     </div>
                 </div>
             </motion.div>
