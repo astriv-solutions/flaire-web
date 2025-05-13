d0056a376f7e64ffbacf44f3717c6d641d61f819
ravmax-rdl <ravindudl.rc@gmail.com>
2025-05-13 12:28:29 +0530

    feat: Add gl-matrix dependency and replace Testimonials component with InfiniteMenu in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Hero from '@/components/Hero';
-import Testimonials from '@/components/Testimonials';
 import Pricing from '@/components/Pricing/Pricing';
 import FAQ from '@/components/FAQ';
 import Logos from '@/components/Logos';
@@ -9,16 +8,18 @@ import Container from '@/components/Container';
 import Section from '@/components/Section';
 import Stats from '@/components/Stats';
 import CTA from '@/components/CTA';
+
 import FlowingMenu from '@/components/FlowingMenu';
 import { flowingmenuitems } from '@/data/flowingmenu';
-
+import InfiniteMenu from '../components/InfiniteMenu';
+import { testimonials } from '@/data/testimonials';
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Hero />
       <Logos />
-      <div style={{ height: '450px', position: 'relative', paddingBottom: '50px'}}>
+      <div style={{ height: '450px', position: 'relative', paddingBottom: '50px' }}>
         <FlowingMenu items={flowingmenuitems} />
       </div>
 
@@ -33,12 +34,13 @@ const HomePage: React.FC = () => {
           <Pricing />
         </Section>
 
-        <Section
-          id="testimonials"
-          title="What Our Clients Say"
-          description="Hear from those who have partnered with us."
-        >
-          <Testimonials />
+        <Section id="testimonials" title="Testimonials" description="What out clients say.">
+          <div
+            className="relative w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw]"
+            style={{ height: '600px' }}
+          >
+            <InfiniteMenu items={testimonials} />
+          </div>
         </Section>
 
         <FAQ />
